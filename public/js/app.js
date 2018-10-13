09bf9a2f83d2ac8d8728277d1450d39d4cee3e83
Matt Byrnes <mattjbyrnes@gmail.com>
2018-10-13 15:04:49 -0400

    Routes working for Doc

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,78 @@
-let newDoc = function () {
-    $('body').replaceWith( 
-        
-        
-        `<header id="docHeader">
-        <section id="docTitle">
-         <img src="images/docs_48dp.png" placeholder="docs" />
-         <input type="text" placeholder="Untitled document" />
-         <ul id="options">
-             <li><button class="mainOption">File</button></li>
-             <li><button class="mainOption">Edit</button></li>
-             <li><button class="mainOption">View</button></li>
-             <li><button class="mainOption">Insert</button></li>
-             <li><button class="mainOption">Format</button></li>
-             <li><button class="mainOption">Tools</button></li>
-             <li><button class="mainOption">Table</button></li>
-             <li><button class="mainOption">Add-Ons</button></li>
-             <li><button class="mainOption">Help</button></li>
-           </ul>
-        </section>
-        <section id="styleOpt">
-          <span></span>
-          <span></span>
-          <span></span>
-        </section>
-   </header>
-    <main id="docSection">  
-        <div id="measure"></div>
-        <div id="docArea"></div>
-     </main>
- 
-     ` );
+const openDoc = function (event) {
+    event.preventDefault();
+    const docId = $(this).attr('id');
+    $.ajax({ url: `/get/${docId}`, method: "GET" }).then(function (dbResponse) {
+        console.log('doc id', dbResponse);
+        const selectedDoc = (
+            `
+            <div class="modal-fullpage">
+                <div class="title-header">
+                <input type="text" placeholder="Untitled document" name="Document Title" id="input-title" value="${dbResponse.docTitle}">
+                </div>
+                <div class="page">
+                <input type="textarea" placeholder="Document Content" name="Document Content" id="input-content" value="${dbResponse.docContent}" />
+                <button type="submit" id="add-button"><i class="fas fa-share"></i></button>
+                </div>
+            </div>
+            `
+        )
+        $('#docForm').html(selectedDoc);
+    })
 }
 
+const loadDocs = function () {
+    $.getJSON('/api/applist')
+        .then(function (data) {
+            const docItem = data.map((element) =>
+                `
+                <a class="open-doc-btn" id='${element._id}'>
+                <div class="docPrev">
 
+                        ${element.docContent}
+                   
+                    <section class="caption">
+                        <span class="captionLabel">
+                        <h2>${element.docTitle}</h2>
+                        <div class="labelGroup">
+                            <i class="fas fa-sticky-note"></i>
+                            <h4> Open October 4, 2018</h4>
+                            <i class="fas fa-ellipsis-v"></i>
+                        </div>
+                        </span>
+                    </section>
+                    <i class="fas fa-times" id='${element._id}'></i>
+                </div>
 
-$('#newPlus').on('click', newDoc);
+                </a>
+                `
+            )
+            $('#gdoclist').html(docItem);
+            $('.open-doc-btn').on('click', openDoc);
+            $('.fa-times').on('click', deleteDoc);
+        })
+};
+
+const createDoc = function (event) {
+    event.preventDefault();
+    const newDocument = {
+        docTitle: $('#input-title').val(),
+        docContent: $('#input-content').val()
+    };
+    $.ajax({ url: '/add', method: 'POST', data: newDocument }).then(function (res) {
+        loadDocs();
+    });
+};
+
+const deleteDoc = function (event) {
+    event.preventDefault();
+    const id = event.target.id;
+    $.ajax({ url: `/delete/${id}`, method: "DELETE" }).then(function () {
+        loadDocs();
+    });
+}
+
+loadDocs();
+
+// Listener submit button
+$('form').on('submit', createDoc);
+
