46c114aab4d833fefe322ca95fb9493a20cdda35
MRobinson <mrobinson928@hotmail.com>
2018-10-17 17:27:56 -0400

    Update

--- public/js/newdoc.js
+++ public/js/newdoc.js
@@ -54,3 +54,67 @@ $('select[name="fontdown"]').change(theFont);
 $('select[name="colordown"]').change(theColor);
 $('select[name="sizedown"]').change(theSize);
 
+// function used to autosave user input
+autoSave = (function () {
+
+  timer = null;
+
+  // function to get user input from the text area
+  function userInput() {
+      element = document.getElementsByClassName("docArea").textContent
+
+      if (element.length <= 0)
+          return null;
+
+      return element[0];
+
+
+  }
+
+  // function to save user input from the text area
+  function save() {
+      doc = userInput();
+      if (doc) {
+            localStorage.setItem("autoSave" + document.location, doc.value)
+            console.log(doc.value)
+      }
+      $('#status').text('All Changes Saved!');
+  }
+
+  // function to restore user input from the text area on reload
+  function restore() {
+      saved = localStorage.getItem("autoSave" + document.location)
+      console.log(saved)
+      doc = userInput();
+      if (saved && doc) {
+          doc.value = saved;
+      }
+  }
+
+  return {
+      // function to start autosave timer
+      start: function () {
+          doc = userInput();
+          restore();
+         
+              timer = null;
+          
+
+          timer = setInterval(save, 5000);
+          $('#status').text('Saving...');
+      }
+  }
+}())
+
+// Starting the autosave function
+$(document).ready(function () {
+  autoSave.start()
+})
+
+
+// function to delete saved user input 
+function deleteSelec(event) {
+  if (event.keyCode === 8) {
+      document.execCommand("delete");
+  }
+}
